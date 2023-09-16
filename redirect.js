14012d0d11d49a1b50cf02b53b3300643ab1cf57
PKXYSEfferlynGroup <145211050+PKXYSEfferlynGroup@users.noreply.github.com>
2023-09-16 22:55:45 +0800

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,5 +1,5 @@
 function goto(){
-  location.href = "https:/111.180.189.175:5000/index.html";
+  location.href = "https://111.180.189.175:5000/index.html";
 }
 window.onload = function(){
   goto();
